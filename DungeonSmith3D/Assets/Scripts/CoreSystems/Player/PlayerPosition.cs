d7b7b6a2e7e1cb7eb2d8272df50cd298dc870e58
verycoolpotato <arandmagson@gmail.com>
2025-10-29 11:38:48 -0700

    Camera movement

--- DungeonSmith3D/Assets/Scripts/CoreSystems/Player/PlayerPosition.cs
+++ DungeonSmith3D/Assets/Scripts/CoreSystems/Player/PlayerPosition.cs
@@ -5,7 +5,7 @@ public class PlayerPosition : MonoBehaviour
     [SerializeField] GameObject Player;
     private void Awake()
     {
-        Player.transform.position = Vector3.zero;
+      
     }
     internal void MovePlayer(Vector3 Direction)
     {
