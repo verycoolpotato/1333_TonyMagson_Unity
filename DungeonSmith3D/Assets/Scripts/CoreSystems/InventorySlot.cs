9803e110e8b1314b25a6a442b85ba4af9d617e85
verycoolpotato <arandmagson@gmail.com>
2025-11-22 10:29:27 -0800

    Popup stuff

--- DungeonSmith3D/Assets/Scripts/CoreSystems/InventorySlot.cs
+++ DungeonSmith3D/Assets/Scripts/CoreSystems/InventorySlot.cs
@@ -10,25 +10,30 @@ public class InventorySlot : MonoBehaviour
     [SerializeField] TextMeshProUGUI Name;
     [SerializeField] TextMeshProUGUI Description;
     [SerializeField] Image Icon;
+    [SerializeField] Image SlotIcon;
 
-
+   
     private Item _item;
     public void Clicked()
     {
-        _item = GameManager.Instance.GamePlayer.PlayerInventory.GetInventory()[SlotIndex];
+       
 
         if (_item == null) return;
+
 
         ToolTipPopup.SetActive(!ToolTipPopup.activeSelf);
         Name.text = _item.Stats.ThisItemName;
         Description.text = _item.Stats.Description;
-       // Icon.sprite = _item.Stats.Icon;
+        Icon.sprite = _item.Stats.Icon;
+      
     }
-
 
     private void OnEnable()
     {
-       
-       
+        _item = GameManager.Instance.GamePlayer.PlayerInventory.GetInventory()[SlotIndex];
+
+        if (_item != null)
+            SlotIcon.sprite = _item.Stats.Icon;
     }
+
 }
